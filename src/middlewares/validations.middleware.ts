0a5b76ced2f883c58b3a3d3b4edd9ab495b499e8
snaeryss <sandarageng23@gmail.com>
2025-09-13 05:06:44 +0700

    menambah fungsi validasi

---
+++ src/middlewares/validations.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction } from "express";
+import Joi from "joi";
+
+export const MValidate = (
+    schema: Joi.ObjectSchema
+) => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+    });
+
+    if (error) {
+      const validateError = error.details.map((detail) => {
+        return Error(detail.message);
+      })[0];
+
+      return next(validateError);
+    }
+
+    next();
+  };
+};
