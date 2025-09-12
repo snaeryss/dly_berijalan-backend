0db3438229ce9bb881f66a8cb693dcd13bb46360
snaeryss <sandarageng23@gmail.com>
2025-09-13 04:24:37 +0700

    chore: add dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
+import dotenv from "dotenv";
 import express from "express";
 import { MErrorHandler } from "./middlewares/error.middleware";
 import authRouter from "./routers/auth.route";
 
+dotenv.config();
 const app = express();
 
 app.use("/api/v1/auth", authRouter);
